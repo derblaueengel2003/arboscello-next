5f5693c47870eb5af0f6263d132776d7024b61b0
derblaueengel2003 <angelo.arboscello@me.com>
2025-11-28 23:20:29 +0100

    Modified link resources

--- src/app/utils/projects.js
+++ src/app/utils/projects.js
@@ -3,12 +3,12 @@ export const projects = [
     title: 'myDesk',
     description:
       'Vacation planning project created for the CS50 Web Programming with Python and JavaScript.',
-    link: 'https://github.com/derblaueengel2003/capstone/',
+    link: 'https://capstone.arboscello.com/',
   },
   {
     title: 'Gestionale m2Square',
     description:
       'A custom Tool I created for managing the Real Estate Workflow from acquisition to billing.',
-    link: 'https://github.com/derblaueengel2003/gestionale-electron',
+    link: 'https://gestionale.arboscello.com/',
   },
 ];
